e0ae3bc6780703e809fe17e98da2f57e628f2f9a
unknown <aadeye302@caledonian.ac.uk>
2026-05-13 02:48:48 +0100

    main

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Build a Fraud Detection AI from Scratch - End to End Machine Learning Project 
+Building a Fraud Detection AI pipeline from Scratch 
 
 
 Min Sys requirements 
@@ -17,3 +17,36 @@ Prod
 - Softare: Spark/Dask
 - Training: XGboost, CatBoost
 - Deployment: FastAPI, Flask or MLflow
+
+
+Cloud
+- AWS - ec2 (g5.4xlarge for GPU, Sagemaker for trainier)
+-Azure: ml studio
+- gcp: co studio
+
+TOols
+dOCKER- For containerization and orchestratuin
+- python: synthetic data production (3.10)
+- kafka: modes can be either zookeeper/careaft mode. confluent cloud
+- spark: real time inference
+- MLFlow: model registry and artifacts handling
+- Minio: For model storage
+- Aifrflow: periodic model training
+- FLOWER: airflow worker and task scheduling
+- Redis: celery message bus
+
+ML Model features
+- real time data consumption from kafka
+- temporal and behavioural feature engineering
+- Class imbalance handling with SMOTE (for handling fraudulent transactions)
+- Hyperparameter tuning with randomized searchCV (we switch our values to see patterns, to get the perfect learning rate)
+- XGBoost classifier with optimized threshold (we can choose any other classifier to use)
+- MLflow experiement tracking 
+- Minio s3 integration for model storage
+- Comprehensive metrics and viz logging 
+
+
+
+
+### Disclaimer 
+- 
---
+++ src/producer/main.py
@@ -0,0 +1,171 @@
+import os
+from confluent_kafka import Producer
+from dotenv import load_dotenv
+import logging
+from faker import Faker
+import random
+
+logging.basicConfig(
+    format= "%(asctime)s - %(levelname)s - %(module)s - %(message)s",
+    level= logging.INFO
+)
+
+logger = logging.getLogger(__name__) 
+load_dotenv(dotenv_path="/app/.env")
+fake = Faker()
+
+
+class  TransactionProducer():
+    def __init__(self):
+        self.bootstrap_servers= os.getenv('KAFKA_BOOSTRAP_SERVERS', 'localhost:9092')
+        self.kafka_username= os.getenv('KAFKA_USERNAME')
+        self.kafka_password= os.getenv('KAFKA_PASSWORD')
+        self.topic = os.getenv('KAFKA_TOPIC', 'transactions')
+        self.running = False
+
+        #confluent kafka config
+
+        self.producer_config = {
+            'boostrap.servers': self.bootstrap_servers,
+            'client.id': 'transaction-producer',
+            'compression.type': 'gzip',
+            'linger.ms': '5',
+            'batch.size': 16384,
+        }
+
+        if self.kafka_username and self.kafka_password:
+            self.producer_config.update({
+                'security.protocol': 'SASL_SSL',
+                'sasl.mechanism': 'PLAIN',
+                'sasl.username': self.kafka_username,
+                'sasl.password': self.kafka_password
+            })
+        else:
+            self.producer_config['security.protocol'] = 'PLAINTEXT'
+        try:
+            self.producer = Producer(self.producer_config)
+            logger.info('Confluent Kafka Producer Initialized Successfully')
+        except Exception as e:
+            logger.error(f"failed to initialize confluent kafka producer :{str(e)}")
+            raise e 
+
+
+        self.compromised_users = set(random.sample(range(1000,9999),50)) #0.5% of users
+        self.high_risk_merchants = ['QuickCash', 'GlobalDigital', 'FastMoneyX' ]
+        self.fraud_pattern_weights = {
+            'account_takeover': 0.4,   #40% of fraud cases thus higher weighting
+            'card_testing': 0.3,  #30% of fraud cases
+            'merchant_collusion':0.2,
+            'geo_anomaly':0.1
+        }
+    
+    def delivery_report(self, err,msg):
+        if err is not None:
+            logger.error(f'Message delivery failed: {err}')
+        else:
+            logger.info(f'message delivered to {msg.topic()} [{msg.partition()}]')
+    def generate_transaction(self):
+        transaction ={
+            'transaction_id': fake.uuid4(),
+            'user_id':random.randint(a:1000, b:9999),
+            'amount': round(fake.pyfloat(min_value=0.01, max_value=1000),2),
+            'currency': 'USD',
+            'merchant': fake.company(),
+            'timestamp': (datetime.now(timezone.utc) + 
+                            timedelta(seconds=random.randint(-300,
+                            3000))).isoformat(),
+            'location': fake.country_code(),
+            'is_fraud':0
+        }
+
+        is_fraud=0
+        amount= transaction['amount']
+        user_id= transaction['user_id']
+        merchant = transaction['merchant']
+
+        #account takeover
+        if user_id in self.compromised_users and amount > 500: 
+            if random.random() < 0.3: #30% chance of fraud in compromsed accounts
+                is_fraud =1
+                transaction['amount'] = random.uniform(500, 5000)
+                transaction['merchant'] = random.choice(self.high_risk_merchants)
+        
+        #card testing
+        if not is_fraud and amount < 2.0:
+            #simulate rapid small transactions
+            if user_id %1000 =0 and random.random() < 0.25:
+                is_fraud = 1
+                transaction['amount'] = round(random.uniform(0.01, 2),2)
+                transaction['location'] = 'US'
+        
+        #merchant collusion
+
+        if not is_fraud and merchant in self.high_risk_merchants:
+            if amount > 3000 and random.random() < 0.15:
+                is_fraud = 1
+                transaction['amount'] = round(random.uniform(300, 1500))
+        
+
+        #geographic anomalies 
+        if not is_fraud: 
+            if user_id % 500 == 0 and random.random() < 0.1:
+                is_fraud =1 
+                transaction['location'] = random.choice
+                
+
+
+
+
+
+
+    def send_transaction(self):
+        try:
+            transaction = self.generate_transaction()
+            if not transaction:
+                return False
+
+            self.producer.producer(
+                self.topic,
+                key= transaction['transaction_id'],
+                value=json.dumps(transaction),
+                callback=self.delivery_report
+            )
+
+            self.producer.poll(0) #trihher callbacks
+            return True
+        except Exception as e:
+            logger.error(f'error producing message: {str(e)}')
+            return False 
+
+    def run_continuous_production(self, interval:float=0.0):
+        """run continuous message prdocution with a graceful shutdown"""
+        self.running = True
+        logger.info('Starting producer for topic %s...', self.topic)
+        try:
+            while self.running:
+                if self.send_transaction():
+                    time.sleep(interval)
+        finally:
+            self.shutdown()
+
+    def shutdown(self, signum=None, frame=None):
+        if self.running:
+            logger.info('Initiating Shutdwon...')
+            self.running = False
+
+            if self.producer:
+                self.producer.flush(timeout=30)
+                self.producer.close()
+
+            logger.info('Producer stopped')
+
+        #configuring graceful shutdown
+        signal.signal(signal.SIGINT, self.shutdown)
+        signal.signal(signal.SIGTERM, self.shutdown)
+
+
+
+
+if __name__ == "__main__":
+    producer = TransactionProducer()
+    producer.run_continuous_production()
